8f26ba5d1f8901976657e8037bca1b9a6cf7d167
Davis Volpe <warning-tintype.1f@icloud.com>
2026-04-05 16:04:35 +0000

    feat: add GitHub Actions workflow for deploying to GitHub Pages

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,20 +1,17 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
-import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite';
+import vue from '@vitejs/plugin-vue';
+import vueDevTools from 'vite-plugin-vue-devtools';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-    vue(),
-    vueDevTools(),
-  ],
+  base: '/python-honors-project/',
+  plugins: [tailwindcss(), vue(), vueDevTools()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
   server: {
@@ -25,4 +22,4 @@ export default defineConfig({
       },
     },
   },
-})
+});
